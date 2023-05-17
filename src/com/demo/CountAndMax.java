b0891326cf546a96fcb7abee281c355358bf9838
javasupercoder <javasupercoder@gmail.com>
2023-05-17 12:47:27 +0530

    countandmax sysout added

--- src/com/demo/CountAndMax.java
+++ src/com/demo/CountAndMax.java
@@ -7,13 +7,13 @@ import java.util.List;
 public class CountAndMax {
 	public static void main(String[] args) {
 		List<Integer> numbers=new ArrayList(Arrays.asList(12,4,6,12,4,77,89));
-		
+		System.out.println("List elements are : "+numbers);
 		long count=numbers.stream().count();
 		
 		System.out.println("Count of numbers:"+count);
 		
 		int max=numbers.stream().max(Integer::compare).get();
-		
+		  
 		System.out.println("Max number = "+max);
 	}
 }
