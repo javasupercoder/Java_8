8ce7e42e175f2a79ae5dcab86a2ae1a370529b4d
javasupercoder <130356943+javasupercoder@users.noreply.github.com>
2023-10-18 10:11:33 +0530

    added 1 code

--- src/com/demo/CountAndMax.java
+++ src/com/demo/CountAndMax.java
@@ -21,6 +21,7 @@ public class CountAndMax {
 		System.out.println("End of code from Github website try 1");
 		System.out.println("End of the code 18 oct");
 		System.out.println("End of the code 18 oct...");
+		System.out.println("How are you.");
 		
 	}
 }
