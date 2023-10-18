29d896f768212b108235069561c2f2b7683cc669
javasupercoder <javasupercoder@gmail.com>
2023-10-18 10:06:47 +0530

    msg1 from 18 oct

--- src/com/demo/CountAndMax.java
+++ src/com/demo/CountAndMax.java
@@ -19,6 +19,8 @@ public class CountAndMax {
 		
 		System.out.println("End of code from Github website");
 		System.out.println("End of code from Github website try 1");
+		System.out.println("End of the code 18 oct");
+		System.out.println("End of the code 18 oct...");
 		
 	}
 }
